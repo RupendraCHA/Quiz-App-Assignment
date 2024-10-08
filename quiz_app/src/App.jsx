4f8f49b6deb1d719453b10d0ebc0db7450ea40c3
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-10-09 02:24:52 +0530

    Variable assignments

--- quiz_app/src/App.jsx
+++ quiz_app/src/App.jsx
@@ -39,9 +39,6 @@ const App = () => {
       setQuestionNumber(parseInt(questionIndex, 10));
       setHide(true)
       setNameInput(false)
-
-    }
-    if (name) {
       setName(name)
     }
   }, []);
